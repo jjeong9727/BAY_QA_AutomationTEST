58a0e7fcd7a962d4e5ed36a42ba9c377c1011fda
Jeongeui <jeongeui9727@google.com>
2025-03-25 13:30:20 +0900

    파일 수정

--- scripts/send_slack.py
+++ scripts/send_slack.py
@@ -3,21 +3,22 @@ import os
 import requests
 from datetime import datetime
 
-
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
-# JIRA_URL = os.getenv("JIRA_URL")
+# 환경 변수에서 Slack Webhook과 Jira URL 가져오기
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+JIRA_URL = os.getenv("JIRA_URL")
 
 def send_slack_message(text):
-    payload = { "text": text }
-    response = requests.post(SLACK_WEBHOOK_URL, json=payload)
+    response = requests.post(SLACK_WEBHOOK_URL, json={"text": text})
     if response.status_code != 200:
         print("Slack 전송 실패:", response.text)
 
 def main():
     now = datetime.now().strftime("%Y-%m-%d %H:%M")
 
+    summary_path = os.path.join("scripts", "summary.json")
+
     try:
-        with open("summary.json", "r", encoding="utf-8") as f:
+        with open(summary_path, "r", encoding="utf-8") as f:
             all_tests = json.load(f)
     except FileNotFoundError:
         send_slack_message(f"⚠️ [{now}] summary.json 파일이 존재하지 않습니다.")
@@ -26,11 +27,11 @@ def main():
     passed_tests = [t for t in all_tests if t["status"] == "passed"]
     failed_tests = [t for t in all_tests if t["status"] == "failed"]
 
-    message = f"📢 [{now}] 테스트 결과 \n"
+    message = f"📢 [{now}] 테스트 결과 요약\n"
 
     # 성공 테스트 목록
     if passed_tests:
-        message += f"\n🟦 성공 테스트 목록:\n"
+        message += f"\n🟩 성공 테스트 목록:\n"
         for i, test in enumerate(passed_tests, 1):
             message += f"{i}. {test['name']}\n"
 
@@ -51,7 +52,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-response = requests.post(SLACK_WEBHOOK_URL, json={"text": test_message})
-print("전송 결과", response.status_code)
