aa75d2e3050323fde75a8cde723fc7bdf3ac008b
Jeongeui <jeongeui9727@google.com>
2025-03-25 14:43:59 +0900

    Jira 등록 수정

--- scripts/create_jira.py
+++ scripts/create_jira.py
@@ -7,7 +7,7 @@ from requests.auth import HTTPBasicAuth
 JIRA_URL = os.getenv("JIRA_URL")
 JIRA_EMAIL = os.getenv("JIRA_EMAIL")
 JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
-PROJECT_KEY = "QA"  # Jira 프로젝트 키
+PROJECT_KEY = os.getenv("JIRA_PROJECTKEY")
 
 def create_issue(summary, description):
     url = f"{JIRA_URL}/rest/api/3/issue"
